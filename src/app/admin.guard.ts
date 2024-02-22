dae37c883f8c4393b87c33702063125801ce672a
NicolasVanDyck <r0878921@student.thomasmore.be>
2024-02-22 15:38:57 +0100

    added authentication and admin guards

--- src/app/admin.guard.ts
+++ src/app/admin.guard.ts
@@ -1,5 +1,3 @@
-//admin.guard.ts
-
 import { inject } from '@angular/core';
 import { CanActivateFn } from '@angular/router';
 import { RoleService } from './role.service';
--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,11 +4,16 @@ import { HomeComponent } from './home/home.component';
 import { ReviewComponent } from './review/review.component';
 import { AuthGuard } from '@auth0/auth0-angular';
 import { AdminComponent } from './admin/admin.component';
+import { adminGuard } from './admin.guard';
 
 const routes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'review', component: ReviewComponent },
-  { path: 'admin', component: AdminComponent },
+  {
+    path: 'admin',
+    component: AdminComponent,
+    canActivate: [AuthGuard, adminGuard],
+  },
 ];
 
 @NgModule({
