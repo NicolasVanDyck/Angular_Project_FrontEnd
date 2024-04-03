bbe239233e7755f43c8fff1ec96689f098ce9612
NicolasVanDyck <r0878921@student.thomasmore.be>
2024-04-03 19:01:25 +0200

    homepage

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,11 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { AuthService } from '@auth0/auth0-angular';
+import { LoginButtonComponent } from '../buttons/login-button/login-button.component';
+import { SignUpButtonComponent } from '../buttons/sign-up-button/sign-up-button.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, LoginButtonComponent, SignUpButtonComponent],
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css'],
 })
-export class HomeComponent {}
+export class HomeComponent {
+  isAuthenticated = signal(false);
+
+  constructor(private auth: AuthService) {
+    this.auth.isAuthenticated$.subscribe((auth) => {
+      this.isAuthenticated.set(auth);
+    });
+  }
+}
